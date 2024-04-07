e889f77c3063da672646352be031c5019beb05b0
Thuận Phát <phat06112003@gmail.com>
2024-04-07 16:05:30 +0700

    Save location history

--- app/src/main/java/com/example/mapsproject/LocationHistory.java
+++ app/src/main/java/com/example/mapsproject/LocationHistory.java
@@ -17,11 +17,14 @@ import androidx.core.content.ContextCompat;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.model.Place;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.database.*;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
@@ -33,6 +36,7 @@ public class LocationHistory extends Service {
     private Handler handler;
     FirebaseFirestore db;
     CollectionReference colRef;
+    private DocumentReference docRef;
 
     @Nullable
     @Override
@@ -45,6 +49,7 @@ public class LocationHistory extends Service {
         super.onCreate();
         this.db= FirebaseFirestore.getInstance();
         this.colRef = db.collection("LocationHistory");
+        this.docRef = db.collection("LocationHistory").document(GlobalVariable.userName);
         handler = new Handler(Looper.getMainLooper());
         handler.postDelayed(locationRunnable, INTERVAL);
     }
@@ -65,28 +70,57 @@ public class LocationHistory extends Service {
             return;
         }
         Location currentLocation = GlobalVariable.myMap.getMyLocation();
-//        Calendar currentTime = Calendar.getInstance();
+        Calendar currentTime = Calendar.getInstance();
         if (currentLocation != null) {
-            GlobalVariable.LocationHistory.add(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()));
+            String saveField = "History";
+            this.docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    if (documentSnapshot.exists()) {
+                        ArrayList<String> historyArray = (ArrayList<String>) documentSnapshot.get(saveField);
 
-            Map<String, Object> data = new HashMap<>();
-            data.put("Latitude", currentLocation.getLatitude());
-            data.put("Longtitude", currentLocation.getLongitude());
-            data.put("DateTime", new Date());
+                        if (historyArray == null) {
+                            historyArray = new ArrayList<>();
+                        }
+                        historyArray.add(currentTime.get(Calendar.DAY_OF_MONTH) + "-" + currentTime.get(Calendar.MONTH) + "-" + currentTime.get(Calendar.YEAR) + " " + currentLocation.getLatitude() + " " + currentLocation.getLongitude());
+                        Map<String, Object> updates = new HashMap<>();
+                        updates.put(saveField, historyArray);
+                        docRef.update(updates).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "Document updated successfully!");
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d(TAG, "Error updating document", e);
+                            }
+                        });
+                    } else {
+                        Log.d(TAG, "Document does not exist, create a new one");
+                        ArrayList<String> historyArray = new ArrayList<>();
+                        historyArray.add(currentTime.get(Calendar.DAY_OF_MONTH) + "-" + currentTime.get(Calendar.MONTH) + "-" + currentTime.get(Calendar.YEAR) + " " + currentLocation.getLatitude() + " " + currentLocation.getLongitude());
 
-            colRef.add(data)
-                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                        @Override
-                        public void onSuccess(DocumentReference documentReference) {
-                            Log.d(TAG, "Document added with ID: " + documentReference.getId());
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.w(TAG, "Error adding document");
-                        }
-                    });
+
+                        Map<String, Object> data = new HashMap<>();
+                        data.put(saveField, historyArray);
+
+                        docRef.set(data)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(TAG, "Document created successfully!");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error creating document", e);
+                                    }
+                                });
+                    }
+                }
+            });
         }
         else {
             Log.d(TAG, "current location is null");
